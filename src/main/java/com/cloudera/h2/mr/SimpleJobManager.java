fae559556ec6a73a9a600ede0aefe6c7cbe6c8df
E. Sammer <esammer@cloudera.com>
2010-09-20 01:20:54 -0400

    - Made SimpleJobManager observable.

--- src/main/java/com/cloudera/h2/mr/SimpleJobManager.java
+++ src/main/java/com/cloudera/h2/mr/SimpleJobManager.java
@@ -4,12 +4,19 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
-public class SimpleJobManager implements JobManager {
+import com.cloudera.h2.mr.util.observer.Observable;
+import com.cloudera.h2.mr.util.observer.Observer;
+import com.cloudera.h2.mr.util.observer.ObserverEvent;
+import com.cloudera.h2.mr.util.observer.ObserverSupport;
+
+public class SimpleJobManager implements JobManager, Observable {
 
   private Map<String, JobAttempt> activeJobs;
+  private ObserverSupport observerSupport;
 
   public SimpleJobManager() {
     activeJobs = new HashMap<String, JobAttempt>();
+    observerSupport = new ObserverSupport();
   }
 
   public void addJobAttempt(JobAttempt jobAttempt) {
@@ -28,7 +35,20 @@ public class SimpleJobManager implements JobManager {
 
     addJobAttempt(attempt);
 
+    observerSupport.dispatchEvent(ObserverEvent.withTypeAndSubject(
+        "jobmanager.SUBMIT_JOB", attempt));
+
     return attempt;
+  }
+
+  @Override
+  public void addObserver(Observer observer) {
+    observerSupport.addObserver(observer);
+  }
+
+  @Override
+  public void removeObserver(Observer observer) {
+    observerSupport.removeObserver(observer);
   }
 
 }
