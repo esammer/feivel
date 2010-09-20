233dad7eb982c0f4887973c86e416118881f5b07
E. Sammer <esammer@cloudera.com>
2010-09-20 00:53:15 -0400

    - Added a test of parameters.

--- src/test/java/com/cloudera/h2/mr/TestJob.java
+++ src/test/java/com/cloudera/h2/mr/TestJob.java
@@ -17,4 +17,25 @@ public class TestJob {
   public void testJob() {
     Assert.assertNotNull(job);
   }
+
+  @Test
+  public void testParameters() {
+    Exception exception;
+
+    job.getParameters().put("job.name", "Test Job");
+
+    Assert.assertEquals("Test Job",
+        job.getParameters().get("job.name", String.class));
+
+    exception = null;
+
+    try {
+      job.getParameters().get("job.name", Long.class);
+    } catch (IllegalArgumentException e) {
+      exception = e;
+    }
+
+    Assert.assertNotNull(exception);
+  }
+
 }
