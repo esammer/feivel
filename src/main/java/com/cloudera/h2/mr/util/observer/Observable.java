07b218c6aebd6970a1807fa04e24e672d5e0df11
E. Sammer <esammer@cloudera.com>
2010-09-30 14:22:52 -0400

    - Removed observer files. Moved them into a separate project.

--- src/main/java/com/cloudera/h2/mr/util/observer/Observable.java
+++
@@ -1,9 +0,0 @@
-package com.cloudera.h2.mr.util.observer;
-
-public interface Observable {
-
-  public void addObserver(Observer observer);
-
-  public void removeObserver(Observer observer);
-
-}
--- src/main/java/com/cloudera/h2/mr/util/observer/Observer.java
+++
@@ -1,8 +0,0 @@
-package com.cloudera.h2.mr.util.observer;
-
-
-public interface Observer {
-
-  public void onEvent(ObserverEvent<?> event) throws ObserverException;
-
-}
